d99c78d651a9a207139410f18f8140547970ab90
ved_patil <vedp02854@gmail.com>
2026-02-17 14:01:46 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>   // Header file for input-output
+
+using namespace std;  // Allows us to use cout without std::
+
+int main() {
+    cout << "Hello, World!" << endl;  // Prints text to screen
+    return 0;  // Indicates successful execution
+}
