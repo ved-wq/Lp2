640d5801ceb2bc2a7f8de9efc09aefb2bca3861b
ved_patil <vedp02854@gmail.com>
2026-05-14 20:08:39 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,3 +6,39 @@ int main() {
     cout << "Hello, World!" << endl;  // Prints text to screen
     return 0;  // Indicates successful execution
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+arr = [1, 2, 3, 4, 5]
+x = 4
+
+low, high = 0, len(arr)-1
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if arr[mid] == x:
+        print("Found at index", mid)
+        break
+    elif arr[mid] < x:
+        low = mid + 1
+    else:
+        high = mid - 1
