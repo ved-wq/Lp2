cbe6e2a79815062e005bae3ad4fde5c1a74d4f1b
ved_patil <vedp02854@gmail.com>
2026-05-14 20:04:23 +0530

    Update program.py

--- program.py
+++ program.py
@@ -1,1 +1,29 @@
 print("hello")
+
+
+
+
+
+
+
+
+
+
+
+# Binary Search (shortest simple version)
+
+arr = [1, 2, 3, 4, 5]
+x = 4
+
+low, high = 0, len(arr)-1
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if arr[mid] == x:
+        print("Found at index", mid)
+        break
+    elif arr[mid] < x:
+        low = mid + 1
+    else:
+        high = mid - 1
